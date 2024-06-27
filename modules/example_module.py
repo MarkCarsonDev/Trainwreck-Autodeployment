c070f294d69e69ae835734562332159e70b110bf
MarkCarsonDev <markcarson.dev@gmail.com>
2024-06-26 21:22:48 -0700

    Refactor example_module.py to remove unnecessary arguments in example_command

--- modules/example_module.py
+++ modules/example_module.py
@@ -1,17 +1,13 @@
 from discord.ext import commands
+import typing
 
 @commands.command()
-async def example_command(ctx, arg1: str, *, everythingafterarg1: str):
+async def example_command(ctx):
     """
     Description under !help
     """
     gif_url = "https://tenor.com/view/dissolve-gif-18144671279500427383"
-    if not arg1:
-        arg1 = ""
-    if not everythingafterarg1:
-        everythingafterarg1 = ""
-        
-    await ctx.send(gif_url, arg1, "\n", everythingafterarg1)
+    await ctx.send(gif_url)
 
 def setup(bot):
     bot.add_command(example_command)
