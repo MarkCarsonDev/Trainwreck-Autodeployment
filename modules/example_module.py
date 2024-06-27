0c1ebb52f37ed5ea940276ee077258fe2d814d31
MarkCarsonDev <markcarson.dev@gmail.com>
2024-06-26 21:16:29 -0700

    feat: Add lazy loading for images in example_module.py

--- modules/example_module.py
+++ modules/example_module.py
@@ -6,6 +6,11 @@ async def example_command(ctx, arg1: str, *, everythingafterarg1: str):
     Description under !help
     """
     gif_url = "https://tenor.com/view/dissolve-gif-18144671279500427383"
+    if not arg1:
+        arg1 = ""
+    if not everythingafterarg1:
+        everythingafterarg1 = ""
+        
     await ctx.send(gif_url, arg1, "\n", everythingafterarg1)
 
 def setup(bot):
